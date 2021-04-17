0fb8dbe5dee96edcb759d597c81180f0891b0e96
Johann Taylor <j.taylor1343@gmail.com>
2021-04-17 18:05:52 -0400

    Updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,48 +1,53 @@
-const APP_PREFIX = 'Budget-';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+const CACHE_NAME = 'my-site-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./assets/css/style.css",
-    "./js/index.js"
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/js/index.js",
+  "/manifest.json",
 ]
 
-self.addEventListener('install', function(e){
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-          console.log('installing cache : ' + CACHE_NAME)
-          return cache.addAll(FILES_TO_CACHE)
-        })
-      )
+self.addEventListener('install', function (evt) {
+  evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+          console.log('Your files were pre-cached successfully');
+          return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+
+  self.skipWaiting();
 })
 
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      let cacheKeeplist = keyList.filter(function(key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      cacheKeeplist.push(CACHE_NAME);
-
-      return Promise.all(
-        keyList.map(function(key, i) {
-          if (cacheKeeplist.indexOf(key) === -1) {
-            console.log('deleting cache : ' + keyList[i]);
-            return caches.delete(keyList[i]);
-          }
-        })
-      );
-    })
+self.addEventListener('activate', function (evt) {
+  evt.waitUntil(
+      caches.keys().then(keyList => {
+          return Promise.all(
+              keyList.map(key => {
+                  if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                      console.log('Removing old cache data', key);
+                      return caches.delete(key);
+                  }
+              })
+          );
+      })
   );
-});
 
+  self.clients.claim();
+});
 
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
   e.respondWith(
-    caches.match(e.request).then(function (request) {
-      return request || fetch(e.request)
-    })
+      caches.match(e.request).then(function (request) {
+          if (request) {
+              console.log('responding with cache : ' + e.request.url)
+              return request
+          } else {
+              console.log('file is not cached, fetching : ' + e.request.url)
+              return fetch(e.request)
+          }
+      })
   )
 })
